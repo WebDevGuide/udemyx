57ea2beec1624096e562cf57b6fa47df891b1de9
Taoufik's Company <114016397+WebDevGuide@users.noreply.github.com>
2023-03-21 10:24:58 +0000

    Update script.js

--- script.js
+++ script.js
@@ -20,38 +20,3 @@ function changeDisplayText() {
 changeDisplayText(); // call on page load
 setInterval(changeDisplayText, 7000);
 
-
-/*hy*/
-
-const scrollDiv = document.getElementById('scroll-div');
-const contactMessage = document.querySelector('.contact-message');
-
-let lastScrollPosition = 0;
-
-window.addEventListener('scroll', function() {
-  const currentScrollPosition = window.pageYOffset;
-  
-  if (currentScrollPosition >= 500 && lastScrollPosition < currentScrollPosition) {
-    scrollDiv.style.display = 'block';
-  } else if (lastScrollPosition > currentScrollPosition) {
-    scrollDiv.style.display = 'none';
-  }
-  
-  lastScrollPosition = currentScrollPosition;
-});
-
-contactMessage.addEventListener('mouseover', function() {
-  document.body.style.cursor = 'pointer';
-});
-
-contactMessage.addEventListener('mouseout', function() {
-  document.body.style.cursor = 'auto';
-});
-
-// 
-    // hide company name 
-    setTimeout(() => {
-      const companyName = document.querySelector('.company-name');
-      companyName.style.bottom = '-20px';
-      companyName.style.opacity = 0;
-    }, 10000);
